22ba642371c3520ddc5312ad92063f0268dbca10
annayrina <annayrina1998@mail.ru>
2023-11-28 18:59:52 +0700

    first commit

---
+++ src/main/java/pro/sky/skyprocoldemo/SkyproColDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprocoldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproColDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproColDemoApplication.class, args);
+	}
+
+}
